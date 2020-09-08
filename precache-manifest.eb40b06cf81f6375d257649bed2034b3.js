bd5e9e9303b56fd18c5d6d91b2f389492d496bce
abhilash1408 <abhilashthot99@gmail.com>
2020-09-09 02:27:01 +0530

    first commit

---
+++ precache-manifest.eb40b06cf81f6375d257649bed2034b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2591f8ec3e91020ff0f01f99946017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c006f27cf75e06e0572",
+    "url": "/static/js/2.72b213bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72b213bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a81e93087d7d9a4b28b6",
+    "url": "/static/js/main.f6475118.chunk.js"
+  },
+  {
+    "revision": "16d50208a04408d1bf24",
+    "url": "/static/js/runtime-main.23041e79.js"
+  }
+]);
